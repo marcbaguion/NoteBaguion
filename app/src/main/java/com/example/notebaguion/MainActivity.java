3733abbadd9bda1032bcf0410189eac60513c541
MARC BAGUION <marcangelo.baguion@gmail.com>
2022-07-08 16:55:21 +0800

    Milestone 1: Add adapter and functions

--- app/src/main/java/com/example/notebaguion/MainActivity.java
+++ app/src/main/java/com/example/notebaguion/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.notebaguion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> notes;
+    ArrayAdapter<String> notes_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListAdapterMethod();
+        btnAddListenerMethod();
+
+
+    }
+    private void btnAddListenerMethod(){
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText etNote = findViewById(R.id.etNote);
+                String note = etNote.getText().toString();
+                notes.add(note);
+                notes_adapter.notifyDataSetChanged();
+                etNote.setText("");
+ //  Log.d("marc", "on click works");
+                // Toast.makeText(getBaseContext(), "Click Add", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+    private void setListAdapterMethod ()  {
+        ListView lvList = findViewById(R.id.lvList);
+        notes = new ArrayList<>();
+        notes.add("First Note");
+        notes.add("Second Note");
+
+        notes_adapter = new ArrayAdapter<>(getBaseContext(),android.R.layout.simple_list_item_1, notes);
+        lvList.setAdapter(notes_adapter);
+
+        notes.add("Marc Angelo Baguion");
+
     }
 }
